f2a2d1109a4ae953c492d76dfc547f21268ed814
skysoon02 <skysoon02@kaist.ac.kr>
2023-10-23 01:26:58 +0900

    init

---
+++ src/ReaderBoard.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+
+function ReaderBoard() {
+  const [rules, setRules] = useState([]);
+  const [update, setUpdate] = useState(0);
+
+  useEffect(()=>{
+    (async()=>{
+      const response = await axios.get("http://13.124.69.102:5002/leaderboard");
+      setRules(response.data.data);
+    })();
+    console.log(rules);
+  }, [update]);
+
+
+
+  return (
+    <div className="App">
+      <h1>LeaderBoard</h1>
+      <table>
+        <tr>
+          <th scope="col">username</th>
+          <th scope="col">ruletitle</th>
+          <th scope="col">rule</th>
+          <th scope="col">score</th>
+          <th scope="col">upvote</th>
+        </tr>
+        {rules.length > 0 && rules.map(rule =>(
+          <tr>
+            <td>{rule.username}</td>
+            <td>{rule.ruletitle}</td>
+            <td><a href={'http://13.124.69.102:5002/cs473_life?rule='+rule.link+'&username='+rule.username}>{rule.link}</a></td>
+            <td>{rule.score}</td>
+            <td><button
+              onClick={()=>{axios.get('http://13.124.69.102:5002/upvote?rule='+rule.link+'&username='+rule.username);setUpdate(update+1)}}
+            >up vote</button></td>
+          </tr>
+        ))}
+        </table>
+    </div>
+  );
+}
+
+export default ReaderBoard;
